f98debcc0288af58a55181ef4cf69a0f5171d122
walddy <walddy02@gmail.com>
2023-09-14 11:11:22 +0200

    commit

--- Práctica 2/js/script.js
+++ Práctica 2/js/script.js
@@ -7,7 +7,22 @@ function inicio() {
   ocument.getElementById("btn5").addEventListener("click", funcion4);
   ocument.getElementById("btn6").addEventListener("click", funcion5);
   function funcion1() {
-    alert("Estoy en la función 1");
+    let numero1 = Math.round(Math.random() * 100);
+    
+    //Primera opción
+    /* document.getElementById("c1").textContent=numero1;
+    document.getElementById("c1").style.backgroundColor="green"; */
+
+    //Segunda opción
+    let caja1 = document.getElementById("c1");
+    caja1.textContent = numero1;
+    caja1.style.backgroundColor = "green";
+
+    //Tercera opción
+    /* with (document.getElementById("c1")) {
+      textContent=numero1;
+      style.backgroundColor="green";
+    } */
   }
   function funcion2() {}
   function funcion3() {}
