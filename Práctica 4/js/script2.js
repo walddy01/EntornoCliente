fd185496ff407c5967aeadd11688aea3867ef9aa
walddy <walddy02@gmail.com>
2023-09-21 12:37:30 +0200

    prettier

--- Práctica 4/js/script2.js
+++ Práctica 4/js/script2.js
@@ -7,7 +7,7 @@ let contenedorPrincipal = document.getElementById("contenedorP");
 
 // Declarar vectores
 const colores = ["Red", "Green", "Blue", "Orange", "Yellow"];
-const nombres = ["Red", "Green", "Blue", "Orange", "Yellow"]; 
+const nombres = ["Red", "Green", "Blue", "Orange", "Yellow"];
 
 // Variables
 let tiempo;
@@ -16,7 +16,7 @@ let puntos = 0;
 
 function inicio() {
   console.log("entro en inicio");
-  
+
   let campo = document.createElement("fieldset");
 
   // Incorporar al nuevo componente creado el estilo de clase .container que está en el CSS
@@ -25,11 +25,11 @@ function inicio() {
   let leyenda = document.createElement("legend");
   leyenda.textContent = "Juegos 21 S";
   campo.appendChild(leyenda);
-  
+
   // Contenedor de los 15 botones de juego
   let contenedorFlex = document.createElement("div");
   contenedorFlex.className = "gallery";
-  
+
   // Crear los 15 botones
   for (let i = 0; i < 15; i++) {
     let divs = document.createElement("div");
@@ -37,38 +37,37 @@ function inicio() {
     divs.setAttribute("Name", "cajasJuego");
     contenedorFlex.appendChild(divs);
   }
-  
+
   // El campo se añade a un elemento padre
   campo.appendChild(contenedorFlex);
 
   // Añadir el campo al contenedor principal
   contenedorPrincipal.appendChild(campo);
-  
+
   btnJugar.onclick = jugar;
 }
 
 function jugar() {
   console.log("Entro en jugar");
-  
+
   contadorSegundos = 30;
   puntos = 0;
   btnJugar.disabled = true;
-  
+
   cargaInfo();
   coloresJuego();
 
   // Temporizador
   tiempo = setInterval(accion, 1000);
-  
+
   function accion() {
     if (contadorSegundos > 0) {
       if (contadorSegundos % 5 == 0) {
-        coloresJuego();  
+        coloresJuego();
       }
-      
+
       contadorSegundos--;
       cargaInfo();
-      
     } else {
       limpiarDivs();
       alert("Fin de partida");
@@ -78,31 +77,31 @@ function jugar() {
 }
 
 function cargaInfo() {
-  spPuntos.textContent = "Puntos: " + puntos + " ----- Tiempo: " + contadorSegundos;
+  spPuntos.textContent =
+    "Puntos: " + puntos + " ----- Tiempo: " + contadorSegundos;
 }
 
 function coloresJuego() {
   let arrayCajasJuego = document.getElementsByName("cajasJuego");
 
   // Foreach
-  arrayCajasJuego.forEach(function(item, posicion) {
+  arrayCajasJuego.forEach(function (item, posicion) {
     let numColorFondo = Math.floor(Math.random() * colores.length);
     let numTextoFondo = Math.floor(Math.random() * nombres.length);
-    
+
     item.style.backgroundColor = colores[numColorFondo];
     item.textContent = nombres[numTextoFondo];
-    
+
     item.onclick = comprobar;
-    
+
     function comprobar() {
       if (numColorFondo == numTextoFondo) {
         puntos++;
-        item.onclick = function() {};
-        
+        item.onclick = function () {};
       } else {
         puntos--;
       }
-      
+
       cargaInfo();
     }
   });
@@ -111,7 +110,7 @@ function coloresJuego() {
 function limpiarDivs() {
   let arrayCajasJuego = document.getElementsByName("cajasJuego");
 
-  arrayCajasJuego.forEach(function(item, posicion) {
-    item.onclick = function() {};
+  arrayCajasJuego.forEach(function (item, posicion) {
+    item.onclick = function () {};
   });
 }
