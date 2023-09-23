653bc0a1c6e8651cf97e329c5e844ea5603bcbe7
walddy <walddy02@gmail.com>
2023-09-23 17:23:12 +0200

    terminada práctica 6

---
+++ Práctica 6/js/script.js
@@ -0,0 +1,41 @@
+window.onload = inicio;
+
+// Identidicar los elementos html
+let btnCargarImagen = document.getElementById("mostrarJS");
+let contenedor = document.getElementById("container");
+let cajaImagenes = document.getElementById("cajaImagenes");
+
+
+function inicio() {
+
+    // Evento click para cargar imágenes
+    btnCargarImagen.addEventListener("click", cargarImagenes)
+
+    function cargarImagenes() {
+
+        // Eliminamos todos los hijos de cajaImagenes
+        while (cajaImagenes.firstChild) {
+            cajaImagenes.removeChild(cajaImagenes.firstChild);
+        }
+
+        for (let i = 0; i < 15; i++) {
+
+            // Genera un número aleatorio para el avatar
+            let num = Math.round(Math.random() * 98);
+
+            // Crea un elemento de imagen
+            let imagenes = document.createElement("img");
+
+            // Establece el atributo src
+            imagenes.setAttribute("src", "https://randomuser.me/api/portraits/men/" + num + ".jpg");
+
+            // Añade la imagen a la caja de imágenes
+            cajaImagenes.appendChild(imagenes);
+
+        }
+
+
+    }
+
+
+}
