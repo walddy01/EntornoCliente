d028a56b20a30184f49f89b536e308d6fcad567c
walddy <walddy02@gmail.com>
2023-10-11 10:19:58 +0200

    commit

---
+++ DWEC_JS/EJ1_12D/ParallaxYosemite/js/script.js
@@ -0,0 +1,5 @@
+window.onload = inicio();
+
+function inicio(){
+    
+}
--- Práctica 17/js/script.js
+++ Práctica 17/js/script.js
@@ -5,11 +5,11 @@ function inicio(){
     let navegacion = document.querySelectorAll("nav ul li a");
     console.log(navegacion);
 
-    let logo = document.querySelectorAll("[href='#myPage']");
+    let logo = document.querySelector("#myPage"); 
     let image = document.createElement("img");
     logo.appendChild(image);
 
-    image.src="./img/appe-touch-icon.png"
+    image.src="Práctica 17/img/apple-touch-icon.png"
 
     let teamDAW = ["Manuel", "Mario", "Mauro", "Álvaro", "Raúl"];
     let enlaceDAW = ["http://practicasmanuelbote.atwebpages.com", "http://raul.blazquez.webcommunity.org", "http://mariojuarez.atwebpages.com", "http://practicasmanuelbote.atwebpages.com", "http://raul.blazquez.webcommunity.org"]
