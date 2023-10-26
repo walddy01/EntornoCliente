115d7779b5cf80c7733c41879112c392431f4654
walddy <walddy02@gmail.com>
2023-10-26 19:34:41 +0200

    Tarea AJAX

--- AJAX/js/script.js
+++ AJAX/js/script.js
@@ -1,49 +1,47 @@
-/*MOSTRAR con javascript CONTENIDOS DE FORMA ASÍNCRONA DE UN FICHERO SITUADO EN UN SERVIDOR */
-window.addEventListener("load",inicio);
+window.onload = inicio;
 let caja = document.getElementById("nose");
 function inicio(){
-              let btnMostrar= document.getElementById("mostrar");
-              btnMostrar.addEventListener("click", mostrar);
-              
-        }  
- function mostrar() {
+  let btnMostrar= document.getElementById("mostrar");
+  btnMostrar.addEventListener("click", mostrar);
+}
 
+function mostrar() {
+console.log("hola");
 /*XMLHttpRequest es un objeto nativo del navegador que permite hacer solicitudes HTTP desde JavaScript*/
-            var xhr = new XMLHttpRequest();
+var xhr = new XMLHttpRequest();
 
 /*define una función que será llamada automáticamente cada vez que cambie su propiedad readystate*/
 
-            xhr.onreadystatechange = cargar;
-            function cargar() {
+xhr.onreadystatechange = cargar;
+function cargar() {
 
 /*readyState: contiene un valor numérico que representa la situación de intercambio de datos a través del objeto.*/
 
 /*status: código numérico devuelto por el servidor. Indica tipo de respuesta a la petición*/
 
-                if (this.readyState == 4 && this.status == 200) {
+    if (this.readyState == 4 && this.status == 200) {
 
 /*respuesta del servidor en formato de texto*/
 
 
-             var objeto = JSON.parse(this.responseText);
-             objeto.foreach(recorrer);
-             function recorrer(item, index){
-               caja.innerHTML += "<p>"+item+"</p>";
-             }
-               }
-             }
-            }
+ var objeto = JSON.parse(this.responseText);
+ objeto.forEach(recorrer);
 
-/* .open: especifica la solicitud
-             - GET/POST.
-             - Archivo: txt, php, xml, json, etc.
-             - true/false: método de envío. */
+   }
+ }
+ function recorrer(item, index) {
+  caja.innerHTML += "<video src='" + item.url + "' autoplay controls></video>";
+}
 
-            xhr.open("GET", "http://camacho.atwebpages.com/jumbotronAleatorio/getImagenes.php", true);
+ /* .open: especifica la solicitud
+ - GET/POST.
+ - Archivo: txt, php, xml, json, etc.
+ - true/false: método de envío. */
+
+ xhr.open("GET", "http://camacho.atwebpages.com/webcam/getWebcam.php", true);
 
 /* .send: envía la solicitud al servidor.
-       Si utilizamos POST debemos pasar los datos por parámetro */
+Si utilizamos POST debemos pasar los datos por parámetro */
 
-            xhr.send();
-       
-        
+xhr.send();
+}
