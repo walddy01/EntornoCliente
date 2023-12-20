ba3b6ce83d785c911412753a40c4bd2f5dde70f3
walddy <walddy02@gmail.com>
2023-12-20 10:11:37 +0100

    commit

--- Angular/listadoCiudades/src/app/app.component.ts
+++ Angular/listadoCiudades/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HttpClient } from "@angular/common/http";
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,13 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Listado';
+
+  pueblitodos:any;
+  constructor(private http: HttpClient) {}
+    ngOnInit (): void {
+      this.http.get("http://camacho.atwebpages.com/jumbotronAleatorio/getImages.php")
+      .subscribe(data => {
+
+      })
+    }
 }
