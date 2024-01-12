1f9bd1a45bfa0f1805b2edffc06715a6f1f65dcb
walddy <walddy02@gmail.com>
2024-01-12 09:22:04 +0100

    a

--- Angular/listadoFrutas/src/app/servicio-f.service.ts
+++ Angular/listadoFrutas/src/app/servicio-f.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import {HttpClient} from '@angular/common/http'
+import { HttpClient } from '@angular/common/http'
 import { Observable } from 'rxjs';
 import { Fruta } from './fruta';
 @Injectable({
@@ -8,7 +8,23 @@ import { Fruta } from './fruta';
 export class ServicioFService {
 
   constructor(private httpCliente: HttpClient) { }
-  leerProductos():Observable<Fruta[]>{
+  leerProductos(): Observable<Fruta[]> {
     return this.httpCliente.get<Fruta[]>('http://moralo.atwebpages.com/menuAjax/productos2/index.php');
-}
+
+  }
+
+  createProduct(producto: Fruta): Observable<Fruta>{
+    console.log("producto"+producto.id)
+        return this.httpCliente.post<Fruta>('http://moralo.atwebpages.com/menuAjax/productos2/create_product.php', producto);
+    }
+  updateProduct(producto: Fruta){
+      return this.httpCliente.put<Fruta>('http://moralo.atwebpages.com/menuAjax/productos2/update_product.php', producto);
+  }
+
+  // deleteProduct(id: string){
+  //     return this.httpClient.delete<Producto>('http://moralo.atwebpages.com/menuAjax/productos2/delete_product.php/?id='+id);
+  // }
+
+
+
 }
